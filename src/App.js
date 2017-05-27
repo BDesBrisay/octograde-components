1e49d95fa9c6b293fa1d37f5114f30358efd2656
dskrenta <dskrenta@gmail.com>
2017-05-27 16:38:13 -0700

    added basic routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {blue500, blue700} from 'material-ui/styles/colors';
-
+import Main from './Main';
 import Header from './Header';
 
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: blue500,
-    primary2Color: blue700,
-  },
-});
-
 const App = () => (
-  <MuiThemeProvider muiTheme={muiTheme}>
+  <div>
     <Header />
-  </MuiThemeProvider>
+    <Main />
+  </div>
 );
 
 export default App
