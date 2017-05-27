b60326db6f99f16061265a73797c84317f048674
dskrenta <dskrenta@gmail.com>
2017-05-27 16:23:40 -0700

    change to functional components

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import {blue500, blue700} from 'material-ui/styles/colors';
 
 import Header from './Header';
 
-export default function App() {
-  const muiTheme = getMuiTheme({
-    palette: {
-      primary1Color: blue500,
-      primary2Color: blue700,
-    },
-  });
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: blue500,
+    primary2Color: blue700,
+  },
+});
 
-  return (
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <Header />
-    </MuiThemeProvider>
-  );
-}
+const App = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Header />
+  </MuiThemeProvider>
+);
+
+export default App
--- src/Header.js
+++ src/Header.js
@@ -7,24 +7,24 @@ import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import ActionHome from 'material-ui/svg-icons/action/home';
 
-export default function Header() {
-  return (
-    <AppBar
-      title='Octograde'
-      titleStyle={{fontFamily: 'Pacifico', textAlign: 'center'}}
-      iconElementLeft={<IconButton><ActionHome /></IconButton>}
-      iconElementRight={
-        <IconMenu
-          iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
-          anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-          targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        >
-          <MenuItem primaryText={'Darth Jar Jar'} />
-          <MenuItem primaryText='Settings' />
-          <MenuItem primaryText='Help' />
-          <MenuItem primaryText='Sign out' />
-        </IconMenu>
-      }
-    />
-  );
-};
+const Header = () => (
+  <AppBar
+    title='Octograde'
+    titleStyle={{fontFamily: 'Pacifico', textAlign: 'center'}}
+    iconElementLeft={<IconButton><ActionHome /></IconButton>}
+    iconElementRight={
+      <IconMenu
+        iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+        anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText={'Darth Jar Jar'} />
+        <MenuItem primaryText='Settings' />
+        <MenuItem primaryText='Help' />
+        <MenuItem primaryText='Sign out' />
+      </IconMenu>
+    }
+  />
+);
+
+export default Header
