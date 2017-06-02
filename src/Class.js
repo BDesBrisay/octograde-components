d3aff79b039c121798ba8a8772ebefd735a520bc
BDesBrisay <bryce@desbrisay.com>
2017-06-01 20:57:27 -0700

    started with the class page

--- src/Class.js
+++ src/Class.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const Class = () => (
-  <h1>Class</h1>
-);
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
+
+import './Class.css';
+
+const classInfo = [
+  {
+    id: 1,
+    title: 'English 110 Period 4',
+    description: 'A regular college level english class',
+    teacher: 'David Skrenta',
+    avatar: 'some url'
+  }
+];
+
+const Class = () => {
+  return (
+    <div>
+      <div className='containerClass'>
+        {classInfo.map(info => (
+          <Subheader style={{marginBottom: 20, marginTop: 50}}>
+            <h1 style={{marginTop: 0, marginBottom: 0}}>{info.title}</h1>
+            <h3 style={{marginTop: 0, marginBottom: 0}}>{info.teacher}</h3>
+            <h3 style={{marginTop: 0, marginBottom: 0}}>{info.description}</h3>
+            <Divider />
+          </Subheader>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Class
