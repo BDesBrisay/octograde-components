30617a7e38770e63f470172905cac4e980727622
BDesBrisay <bryce@desbrisay.com>
2017-06-02 00:17:30 -0700

    Added class stream menu

--- src/Class.js
+++ src/Class.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 import Subheader from 'material-ui/Subheader';
 import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+import ActionList from 'material-ui/svg-icons/action/list';
+import FilterList from 'material-ui/svg-icons/content/filter-list';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+import People from 'material-ui/svg-icons/social/people';
+import ActionSettings from 'material-ui/svg-icons/action/settings';
 
 import './Class.css';
 
@@ -15,18 +24,65 @@ const classInfo = [
   }
 ];
 
+const style = {
+  paper: {
+    display: 'inline-block',
+    float: 'left',
+    margin: '0 32px 16px 0',
+  },
+};
+
+const menuList = {
+  items: [
+    {
+      primaryText: 'Stream',
+      leftIcon: <ActionList />,
+    },
+    {
+      primaryText: 'Filter',
+      leftIcon: <FilterList />,
+    },
+    {
+      primaryText: 'Grades',
+      leftIcon: <ActionGrade />,
+    },
+    {
+      primaryText: 'Classmates',
+      leftIcon: <People />,
+    },
+    {
+      primaryText: 'Class Settings',
+      leftIcon: <ActionSettings />,
+    },
+  ]
+}
+
 const Class = () => {
   return (
     <div>
       <div className='containerClass'>
         {classInfo.map(info => (
-          <Subheader style={{marginBottom: 20, marginTop: 50}}>
-            <h1 style={{marginTop: 0, marginBottom: 0}}>{info.title}</h1>
-            <h3 style={{marginTop: 0, marginBottom: 0}}>{info.teacher}</h3>
-            <h3 style={{marginTop: 0, marginBottom: 0}}>{info.description}</h3>
+          <Subheader className='classInfoCont'>
+            <h1>{info.title}</h1>
+            <h4>{info.teacher}</h4>
+            <h3>{info.description}</h3>
             <Divider />
           </Subheader>
         ))}
+        <div className='classBody'>
+          <div className='classMenu'>
+            <Paper style={style.paper}>
+              <Menu autoWidth={true}>
+                {menuList.items.map(item => (
+                  <MenuItem primaryText={item.primaryText} leftIcon={item.leftIcon} />
+                ))}
+              </Menu>
+            </Paper>
+          </div>
+          <div className='classStream'>
+
+          </div>
+        </div>
       </div>
     </div>
   );
