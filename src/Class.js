1966d57b6214f671e20e75ad6ddf3e23941bb169
BDesBrisay <bryce@desbrisay.com>
2017-06-06 00:07:03 -0700

    updated class stream

--- src/Class.js
+++ src/Class.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import ClassStream from './ClassStream';
+import { SpeedDial, BubbleList, BubbleListItem } from 'react-speed-dial';
+import Avatar from 'material-ui/Avatar';
+import { blue500 } from 'material-ui/styles/colors';
+import { Link } from 'react-router-dom';
 
 import ActionList from 'material-ui/svg-icons/action/list';
 import FilterList from 'material-ui/svg-icons/content/filter-list';
 import ActionGrade from 'material-ui/svg-icons/action/grade';
 import People from 'material-ui/svg-icons/social/people';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import CreateContent from 'material-ui/svg-icons/content/create';
+import Forum from 'material-ui/svg-icons/communication/forum';
 
 import './Class.css';
 
@@ -24,14 +30,6 @@ const classInfo = [
     avatar: 'some url'
   }
 ];
-
-const style = {
-  paper: {
-    display: 'inline-block',
-    float: 'left',
-    margin: '0 32px 16px 0',
-  },
-};
 
 const menuList = {
   items: [
@@ -58,21 +56,45 @@ const menuList = {
   ]
 }
 
+const actionList = {
+	items: [
+		{
+			primaryText: 'Create Post',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<ActionList />} />,
+		},
+		{
+			primaryText: 'Create Assignment',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<ActionSettings />} />,
+		},
+    {
+			primaryText: 'Create Assignment',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<ActionSettings />} />,
+		},
+    {
+			primaryText: 'Create Discussion',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<Forum />} />,
+		},
+    {
+			primaryText: 'Create Assignment',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<ActionSettings />} />,
+		},
+	],
+};
+
 const Class = () => {
   return (
     <div>
       <div className='containerClass'>
-        {classInfo.map((info, index) => (
-          <Subheader key={index} className='classInfoCont'>
-            <h1>{info.title}</h1>
-            <h4>{info.teacher}</h4>
-            <h3>{info.description}</h3>
-            <Divider />
-          </Subheader>
-        ))}
         <div className='classBody'>
           <div className='classMenu'>
-            <Paper style={style.paper}>
+            <Paper className='paperMenu'>
+              {classInfo.map((info, index) => (
+                <div key={index} className='classHeader'>
+                  <h2>{info.title}</h2>
+                  <h4>{info.teacher}</h4>
+                  <h3>{info.description}</h3>
+                </div>
+              ))}
               <Menu autoWidth={true}>
                 {menuList.items.map((item, index) => (
                   <MenuItem key={index} primaryText={item.primaryText} leftIcon={item.leftIcon} />
@@ -83,6 +105,19 @@ const Class = () => {
           <ClassStream />
         </div>
       </div>
+      <SpeedDial hasBackdrop={false}>
+  			<BubbleList>
+  				{actionList.items.map((item, index) => {
+  					return (
+              <BubbleListItem
+                key={index}
+                primaryText={item.primaryText}
+                rightAvatar={item.rightAvatar}
+              />
+            );
+  				})}
+  			</BubbleList>
+  		</SpeedDial>
     </div>
   );
 };
--- src/ClassStream.js
+++ src/ClassStream.js
@@ -2,35 +2,141 @@ import React from 'react';
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-import Divider from 'material-ui/Divider';
+import Chip from 'material-ui/Chip';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+
+import Search from 'material-ui/svg-icons/action/search';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import FilterList from 'material-ui/svg-icons/content/filter-list';
 
 import './Class.css';
 
 const avatarLink = "http://pm1.narvii.com/6028/1e3c8d53a71135affe2a1ecdec7526cdfde912e9_128.jpg";
 
-const ClassStream = () => {  
+const classInfo = [
+  {
+    id: 1,
+    title: 'English 110 Period 4',
+    description: 'A regular college level english class',
+    teacher: 'David Skrenta',
+    avatar: 'some url'
+  }
+];
+
+const styles = {
+  chip: {
+    margin: 4,
+    display: 'inline-block',
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: '0 10px',
+  },
+  turnInButton: {
+    margin: '0 0 0 8px',
+    float: 'right',
+  },
+}
+
+const chips = [
+  { key: 1, text: 'Unit 4' },
+  { key: 2, text: 'Economics' },
+  { key: 3, text: 'Free Trade' },
+  { key: 4, text: 'Globalization' },
+]
+
+/*
+  <div style={styles.wrapper}
+    chips.map((item) => {
+      return (
+        <Chip key={item.key} style={styles.chip}>
+          {item.text}
+        </Chip>
+      );
+    })
+  </div>
+*/
+
+const assignmentNames = [
+  'Demand',
+  'Supply',
+  'Equlibrium',
+  'Price Floor',
+  'Price Ceiling',
+]
+
+const ClassStream = () => {
   return (
-    <div className="classStream">
-    <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
-      <CardHeader
-        title="URL Avatar"
-        subtitle="Subtitle"
-        avatar={avatarLink}
-        actAsExpander={true}
-        showExpandableButton={true}
-      />
-      <CardTitle title="Card title" subtitle="Card subtitle" expandable={true} />
-      <CardText expandable={true}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-        Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-        Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-      </CardText>
-      <CardActions>
-        <FlatButton label="Expand" onTouchTap={this.handleExpand} />
-        <FlatButton label="Reduce" onTouchTap={this.handleReduce} />
-      </CardActions>
-    </Card>
+    <div className='classStream'>
+      <Toolbar style={{marginBottom: 20}}>
+        <ToolbarGroup>
+          <TextField
+            hintText='Search'
+          />
+          <IconButton>
+            <Search />
+          </IconButton>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <FlatButton
+            label="Filter"
+            icon={<FilterList />}
+          />
+          <IconMenu
+            iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+            targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          >
+            <MenuItem primaryText="Refresh" />
+            <MenuItem primaryText="Send feedback" />
+            <MenuItem primaryText="Settings" />
+            <MenuItem primaryText="Help" />
+            <MenuItem primaryText="Sign out" />
+          </IconMenu>
+        </ToolbarGroup>
+      </Toolbar>
+      <Card className='level1'>
+        <CardText actAsExpander={true} className='streamCardCont'>
+          <div className='cardInit'>
+            <div className='cardInitItem'>
+              <h1>10/12</h1>
+            </div>
+            <div className='cardInitItem'>
+              <h1>10/12</h1>
+            </div>
+            <div className='cardInitItem'>
+              <h1>10/12</h1>
+            </div>
+          </div>
+        </CardText>
+        <CardHeader
+          title='URL Avatar'
+          subtitle='Subtitle'
+          avatar={avatarLink}
+          expandable={true}
+        />
+        <CardTitle
+          title='Card title'
+          subtitle='Card subtitle'
+          expandable={true}
+        />
+        <CardText expandable={true}>
+          <div style={{width: '100%', height: 30, backgroundColor: 'red'}}></div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions expandable={true}>
+          <FlatButton label="Reduce" />
+          <FlatButton style={styles.turnInButton} label="Turn In" />
+        </CardActions>
+      </Card>
     </div>
   );
 };
