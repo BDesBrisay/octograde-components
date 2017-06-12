b48ff00a6e668ebb93c45c57fe68f346886f2a7e
BDesBrisay <bryce@desbrisay.com>
2017-06-11 20:54:19 -0700

    Changed toolbar, working on class stream

--- src/Class.js
+++ src/Class.js
@@ -10,6 +10,11 @@ import Avatar from 'material-ui/Avatar';
 import { blue500 } from 'material-ui/styles/colors';
 import { Link } from 'react-router-dom';
 import { List, ListItem } from 'material-ui';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import TextField from 'material-ui/TextField';
 
 import ActionList from 'material-ui/svg-icons/action/list';
 import FilterList from 'material-ui/svg-icons/content/filter-list';
@@ -18,7 +23,8 @@ import People from 'material-ui/svg-icons/social/people';
 import ActionSettings from 'material-ui/svg-icons/action/settings';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import CreateContent from 'material-ui/svg-icons/content/create';
-import Forum from 'material-ui/svg-icons/communication/forum';
+import LiveHelp from 'material-ui/svg-icons/communication/live-help';
+import Chat from 'material-ui/svg-icons/communication/chat';
 import Beenhere from 'material-ui/svg-icons/maps/beenhere';
 import Folder from 'material-ui/svg-icons/file/folder';
 import Email from 'material-ui/svg-icons/communication/email';
@@ -27,6 +33,8 @@ import Help from 'material-ui/svg-icons/action/help';
 import Announcement from 'material-ui/svg-icons/action/announcement';
 import Assessment from 'material-ui/svg-icons/action/assessment';
 import Assignment from 'material-ui/svg-icons/action/assignment';
+import Search from 'material-ui/svg-icons/action/search';
+import Refresh from 'material-ui/svg-icons/navigation/refresh';
 
 import './Class.css';
 
@@ -77,16 +85,16 @@ const menuList = {
 const actionList = {
 	items: [
 		{
-			primaryText: 'Create Announcement',
-			rightAvatar: <Avatar backgroundColor={blue500} icon={<Announcement />} />,
+			primaryText: 'Create Question',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<LiveHelp />} />,
 		},
     {
 			primaryText: 'Create Assessment',
 			rightAvatar: <Avatar backgroundColor={blue500} icon={<Assessment />} />,
 		},
     {
-			primaryText: 'Create Discussion',
-			rightAvatar: <Avatar backgroundColor={blue500} icon={<Forum />} />,
+			primaryText: 'Create Announcement',
+			rightAvatar: <Avatar backgroundColor={blue500} icon={<Chat />} />,
 		},
     {
 			primaryText: 'Create Assignment',
@@ -100,6 +108,42 @@ const Class = () => {
     <div>
       <div className='containerClass'>
         <div className='classBody'>
+          <Toolbar style={{marginBottom: 20}}>
+            <ToolbarGroup>
+              <TextField
+                hintText='Search'
+                fullWidth={true}
+                style={{minWidth: 100,}}
+              />
+              <IconButton>
+                <Search />
+              </IconButton>
+            </ToolbarGroup>
+            <ToolbarGroup>
+              <IconMenu
+                iconButtonElement={
+                  <FlatButton
+                    label="Filter"
+                    icon={<FilterList />}
+                  />
+                }
+                anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                targetOrigin={{horizontal: 'right', vertical: 'top'}}
+              >
+                <MenuItem primaryText="Refresh" />
+                <MenuItem primaryText="Send feedback" />
+                <MenuItem primaryText="Settings" />
+                <MenuItem primaryText="Help" />
+                <MenuItem primaryText="Sign out" />
+              </IconMenu>
+              <IconButton
+                tooltip='Refresh Stream'
+                tooltipPosition='bottom-left'
+              >
+                <Refresh />
+              </IconButton>
+            </ToolbarGroup>
+          </Toolbar>
           <div className='classMenu'>
             <Paper className='paperMenu'>
               {classInfo.map((info, index) => (
--- src/ClassStream.js
+++ src/ClassStream.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import Chip from 'material-ui/Chip';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
@@ -63,38 +62,48 @@ const chips = [
   { key: 4, text: 'Globalization' },
 ]
 
-const assignments = {
-  items: [
-    {
-      id: 1,
-      name: 'Demand Worksheet',
-      dueDate: 'June 15',
-      grade: 'N/A',
-      turnedIn: false,
-    },
-    {
-      id: 2,
-      name: 'Graph Quiz',
-      dueDate: 'June 3',
-      grade: 'N/A',
-      turnedIn: true,
-    },
-    {
-      id: 3,
-      name: 'Supply Worksheet',
-      dueDate: 'June 3',
-      grade: '19/20',
-      turnedIn: true,
-    },
-    {
-      id: 4,
-      name: 'Demand Worksheet',
-      dueDate: 'May 25',
-      grade: '19/20',
-      turnedIn: true,
-    },
-  ]
-};
+const assignments = [
+  {
+    id: 1,
+    name: 'Demand Worksheet',
+    dueDate: 'June 15',
+    grade: 'N/A',
+    turnedIn: false,
+  },
+  {
+    id: 2,
+    name: 'Graph Quiz',
+    dueDate: 'June 3',
+    grade: 'N/A',
+    turnedIn: true,
+  },
+  {
+    id: 3,
+    name: 'Supply Worksheet',
+    dueDate: 'June 3',
+    grade: '19/20',
+    turnedIn: true,
+  },
+  {
+    id: 4,
+    type: 'question',
+    name: 'Demand Worksheet',
+    dueDate: 'null',
+    creationDate: 'May 25, 2017 11:23:04',
+    data: [
+      {
+        type: 'url',
+        title: 'some resource',
+        link: 'some link'
+      },
+      {
+        type: 'file',
+        title: 'some resource',
+        link: 'some link'
+      }
+    ]
+  }
+];
 
 /*
 <div style={styles.wrapper}>
@@ -111,160 +120,11 @@ const assignments = {
 const ClassStream = () => {
   return (
     <div className='classStream'>
-      <Toolbar style={{marginBottom: 20}}>
-        <ToolbarGroup>
-          <TextField
-            hintText='Search'
-            fullWidth={true}
-            style={{minWidth: 100,}}
-          />
-          <IconButton>
-            <Search />
-          </IconButton>
-        </ToolbarGroup>
-        <ToolbarGroup>
-          <IconMenu
-            iconButtonElement={
-              <FlatButton
-                label="Filter"
-                icon={<FilterList />}
-              />
-            }
-            anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
-            targetOrigin={{horizontal: 'right', vertical: 'top'}}
-          >
-            <MenuItem primaryText="Refresh" />
-            <MenuItem primaryText="Send feedback" />
-            <MenuItem primaryText="Settings" />
-            <MenuItem primaryText="Help" />
-            <MenuItem primaryText="Sign out" />
-          </IconMenu>
-          <IconButton
-            tooltip='Refresh Stream'
-            tooltipPosition='bottom-left'
-          >
-            <Refresh />
-          </IconButton>
-        </ToolbarGroup>
-      </Toolbar>
 
-      <Table
-        fixedHeader={false}
-        fixedFooter={false}
-      >
-        <TableHeader
-          adjustForCheckbox={false}
-          displaySelectAll={false}
-          enableSelectAll={false}
-        >
-          <TableRow>
-            <TableHeaderColumn tooltip="The Name">Name</TableHeaderColumn>
-            <TableHeaderColumn tooltip="The Due Date">Due Date</TableHeaderColumn>
-            <TableHeaderColumn tooltip="The Grade">Grade</TableHeaderColumn>
-            <TableHeaderColumn tooltip="The Status">Status</TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody
-          displayRowCheckbox={false}
-        >
-          {assignments.items.map((item) => {
-            return(
-              <TableRow key={item.id}>
-                <TableRowColumn>
-                  <div className='tableItemCont'><Assignment />{item.name}</div>
-                </TableRowColumn>
-                <TableRowColumn>
-                  <div className='tableItemCont'><Today />{item.dueDate}</div>
-                </TableRowColumn>
-                <TableRowColumn>
-                  <div className='tableItemCont'><Grade /> {item.grade}</div>
-                </TableRowColumn>
-                <TableRowColumn>
-                  {item.turnedIn ?
-                    <div className='cardInitItem itemDone'><Done /> Done</div>
-                    :
-                    <div className='cardInitItem itemLate'><PriorityHigh /> Late</div>
-                  }
-                </TableRowColumn>
-              </TableRow>
-            );
-          })};
-        </TableBody>
-      </Table>
-
-      <Subheader style={{marginBottom: 10}}>
-        <div className='cardInit'>
-          <div className='cardInitItem'>
-            Title
-          </div>
-          <div className='cardInitItem'>
-            Due Date
-          </div>
-          <div className='cardInitItem'>
-            Grade
-          </div>
-          <div className='cardInitItem'>
-            State
-          </div>
-          <div className='expanderIcon'>
-          </div>
-        </div>
-      </Subheader>
-
-      {assignments.items.map((item) => {
+      {assignments.items.map((item, index) => {
         return(
-        <Card key={item.id} className='level1 assignmentCard'>
-          <CardText actAsExpander={true} className='streamCardCont'>
-            <div className='cardInit'>
-              <div className='cardInitItem'>
-                <Assignment /> {item.name}
-              </div>
-              <div className='cardInitItem'>
-                <Today /> {item.dueDate}
-              </div>
-              <div className='cardInitItem'>
-                <Grade /> {item.grade}
-              </div>
-              {item.turnedIn ?
-                <div className='cardInitItem itemDone'><Done /> Done</div>
-                :
-                <div className='cardInitItem itemLate'><PriorityHigh /> Late</div>
-              }
-              <div className='expanderIcon'>
-                <ExpandMore />
-              </div>
-            </div>
-          </CardText>
-          <CardHeader
-            title='URL Avatar'
-            subtitle='Subtitle'
-            avatar={avatarLink}
-            expandable={true}
-          />
-          <CardTitle
-            title='Card title'
-            subtitle='Card subtitle'
-            expandable={true}
-          />
-          <CardText expandable={true}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
-          </CardText>
-          <div style={styles.wrapper} expandable={true}>
-            {chips.map((item) => {
-              return(
-                <Chip key={item.key} style={styles.chip}>
-                  {item.text}
-                </Chip>
-              )
-            })}
-          </div>
-          <CardActions expandable={true}>
-            <FlatButton label="Add/Create Content" />
-            <FlatButton style={styles.turnInButton} label="Turn In" />
-          </CardActions>
+        <Card key={index} className='level1 assignmentCard'>
+          <CardText></CardText>
         </Card>
         )
       })}
